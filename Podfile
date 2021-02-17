91bff111b6cc1a06e0866e059b9a5c9ba076aaaa
Vicente Maroto <vicente.maroto@escuelaestech.es>
2021-02-17 20:42:31 +0100

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'catch_jellyfsh' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod  'Each', '~> 1.2'
+  # Pods for catch_jellyfsh
+
+end
---
+++ catch_jellyfsh/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  catch_jellyfsh
+//
+//  Created by A4-iMAC01 on 17/02/2021.
+//
+
+import UIKit
+import ARKit
+import Each
+class ViewController: UIViewController {
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var labelTemp: UILabel!
+    var temp = Each(1).seconds
+    var cuenta = 10
+    let config = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.session.run(config)
+        let tap:UIGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(sender:)))
+        self.sceneView.addGestureRecognizer(tap)
+    }
+    @objc func handleTap(sender: UITapGestureRecognizer) -> Void {
+        print("You Tapped")
+        let sceneViewTappedOn:SCNView = sender.view as! SCNView
+        let touchCoordinates:CGPoint = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        if hitTest.isEmpty {
+            print("No has tocado nada")
+        }else{
+            let results:SCNHitTestResult? = hitTest.first;
+            guard let node = results?.node else {return}
+            print("Has tocado el objeto \(node.name ?? "Unknown Node")")
+            if node.animationKeys.isEmpty,node.name != nil {
+                SCNTransaction.begin()
+                self.animateNode(node: node)
+                SCNTransaction.completionBlock = {
+                    node.removeFromParentNode()
+                    self.addNode()
+                    self.reiniciaTiempo()
+                }
+                SCNTransaction.commit()
+            }
+        }
+    }
+    
+    @IBAction func play(_ sender: Any) {
+        self.addNode()
+        self.setTiempo()
+        self.playButton.isEnabled = false
+    }
+    func reiniciaTiempo() -> Void {
+        self.cuenta = 10
+        self.labelTemp.text = String(self.cuenta)
+    }
+    func setTiempo(){
+        self.temp.perform{ () -> NextStep in
+            self.cuenta -= 1
+            self.labelTemp.text = String(self.cuenta)
+            if self.cuenta == 0{
+                self.labelTemp.text = "Has perdido"
+                return .stop
+            }
+            return .continue
+        }
+    }
+    func addNode() {
+        print("Adding Node")
+        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+        node.name = "Cubo Blanco"
+        node.position = SCNVector3(0, 0, 1)
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+        let node2 = SCNNode(geometry: SCNPyramid(width: 0.2, height: 0.2, length: 0.2))
+        node2.name = "Piramide"
+        node2.position = SCNVector3(0, 0, 0)
+        self.sceneView.scene.rootNode.addChildNode(node2)
+        
+        let jellyScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
+        let jellyNode = jellyScene?.rootNode.childNode(withName: "Jellyfish", recursively: false)
+        jellyNode?.position = SCNVector3(0,1,0)
+        jellyNode?.name = "Medusa"
+        self.sceneView.scene.rootNode.addChildNode(jellyNode!)
+    }
+    func animateNode(node:SCNNode){
+        let spin = CABasicAnimation(keyPath: "position")
+        let pos = node.presentation.position
+        spin.fromValue = pos
+        spin.toValue = SCNVector3(pos.x+1, pos.y, pos.z-1)
+        spin.duration = 0.07
+        spin.autoreverses = true
+        spin.repeatCount = 5
+        node.addAnimation(spin, forKey: "position")
+    
+    }
+}
+
